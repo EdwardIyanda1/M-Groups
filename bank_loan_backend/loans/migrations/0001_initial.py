5eb22e54c806e61834526e77d8efd4cccecfa6a0
Edward Iyanda <edwardiyanda60@gmail.com>
2026-06-10 12:32:36 +0100

    feat: Enhance Admin and Dashboard pages with improved loan display and error handling

--- bank_loan_backend/loans/migrations/0001_initial.py
+++ bank_loan_backend/loans/migrations/0001_initial.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('auth', '0021_remove_user_date_of_birth_remove_user_is_pro_and_more'),
+        ('auth', '0012_alter_user_first_name_max_length'),
     ]
 
     operations = [
