8b5e3d567aca2a67e3b11f6082e4ff90c750a0e9
Edward Iyanda <edwardiyanda60@gmail.com>
2026-06-10 12:30:32 +0100

    feat: Enhance Admin and Dashboard pages with improved loan display and error handling

--- bank_loan_backend/bank_loan_backend/settings.py
+++ bank_loan_backend/bank_loan_backend/settings.py
@@ -56,12 +56,12 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',       # ← after SecurityMiddleware
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
